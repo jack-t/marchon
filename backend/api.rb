dedd378f0ce1f4c437281ee3369e4e557cd4aa5c
jack-t <jackmaverick1@gmail.com>
2017-02-11 13:56:32 -0500

    Initial Backend Commit

---
+++ backend/api.rb
@@ -0,0 +1,39 @@
+require "sinatra"
+require "json"
+require_relative "march.rb"
+
+enable :sessions
+
+get '/marches' do
+    puts March.all
+    March.all.to_json
+end
+
+post '/login' do
+    @username = params[:username]
+    @user = Organizer.first(:username => @username)
+    content_type :json
+    {:id=>@user.id}.to_json
+end
+
+get '/notifications/:march' do
+    @march = March.get(params[:march])
+    @notifs = @march.notifications
+    @notifs.to_json
+end
+
+get '/events/:march' do
+    @march = March.get(params[:march])
+    @events = @march.events
+    @events.to_json
+end
+
+post '/post_notification' do
+    @march = March.get(params[:march]) 
+    @notification = @march.notifications.create(
+        :title => params[:title],
+        :description => params[:description]            
+    )
+    @notification.save!
+     
+end
---
+++ backend/march.rb
@@ -0,0 +1,68 @@
+require "data_mapper"
+require "bcrypt"
+
+DataMapper.setup(:default, 'sqlite::memory:')
+
+class March
+    include DataMapper::Resource
+
+    property :id, Serial
+    property :title, String
+    property :lat, Decimal 
+    property :long, Decimal 
+    property :date, Date 
+
+    has n, :events
+    has n, :notifications
+
+end
+
+class Event
+    include DataMapper::Resource
+
+    property :id, Serial
+    property :title, String
+    property :description, String
+    property :lat, Decimal
+    property :long, Decimal
+    property :time, DateTime
+    belongs_to :march # FK
+end
+
+class Notification
+    include DataMapper::Resource
+
+    property :id, Serial
+    property :title, String
+    property :description, String
+     
+    belongs_to :march # FK
+end
+
+class Organizer
+    include DataMapper::Resource
+    include BCrypt
+    property :id, Serial
+    property :username, String
+
+end
+
+DataMapper.finalize # finish up the models
+DataMapper.auto_upgrade!
+# Setup initial data
+
+march = March.create(
+    :title => "March Title",
+    :lat => 100.0005,
+    :long => 200,
+    :date => Time.now
+)
+march.save!
+
+event = march.events.create(:title => "title", :description => "description")
+event.save!
+
+user = Organizer.create(
+    :username => "username"
+)
+user.save!
